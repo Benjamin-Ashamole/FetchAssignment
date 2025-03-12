181fd8897b965fcaa20dc09235a78d3f330e4282
Benjamin Ashamole <benjiashamole@gmail.com>
2025-03-12 19:13:33 -0400

    Initial Commit

---
+++ FetchTakeHomeAssignment/FetchTakeHomeAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchTakeHomeAssignmentApp.swift
+//  FetchTakeHomeAssignment
+//
+//  Created by Benjamin Ashamole on 3/12/25.
+//
+
+import SwiftUI
+
+@main
+struct FetchTakeHomeAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
